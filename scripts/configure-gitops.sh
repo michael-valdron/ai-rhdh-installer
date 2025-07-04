dd05b74224c5acd22dc2bd982387ee30eda21054
Michael Valdron <mvaldron@redhat.com>
2025-07-04 14:26:54 -0400

    fix up configure-gitops script
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- scripts/configure-gitops.sh
+++ scripts/configure-gitops.sh
@@ -1,53 +1,39 @@
 #!/bin/bash
 
+RHDH_INSTANCE_PROVIDED=${RHDH_INSTANCE_PROVIDED:-false}
 ARGOCD_INSTANCE_PROVIDED=${ARGOCD_INSTANCE_PROVIDED:-false}
 NAMESPACE=${NAMESPACE:-"ai-rhdh"}
-RHDH_DEPLOYMENT="backstage-ai-rh-developer-hub" # deployment created by rhdh operator by default
-DEFAULT_PLUGIN_CONFIGMAP="backstage-dynamic-plugins-ai-rh-developer-hub" # configmap created by rhdh operator for plugins by default
-DEFAULT_SECRET_NAME="rhdh-argocd-secret"
 
+RHDH_ARGOCD_SECRET='rhdh-argocd-secret'
 BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))/.."
 
-# ArgoCD Instance Created By Installer
-if [[ $ARGOCD_INSTANCE_PROVIDED == "false" ]]; then
-    # Add ConfigMap To Configure ArgoCD
-    kubectl apply -n $NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
-fi
-
 EXISTING_NAMESPACE=${EXISTING_NAMESPACE:-''}
-EXISTING_DEPLOYMENT=${EXISTING_DEPLOYMENT:-''}
-RHDH_PLUGINS=${RHDH_PLUGINS:-''}
 ARGO_USERNAME=${ARGO_USERNAME:-''}
 ARGO_PASSWORD=${ARGO_PASSWORD:-''}
 ARGO_HOSTNAME=${ARGO_HOSTNAME:-''}
 ARGO_TOKEN=${ARGO_TOKEN:-''}
 
-# ArgoCD Instance Brought By User
-# If a user is using this step it is assumed they did not use our installer so we need ALL the information required with no assumptions
-if [[ $ARGOCD_INSTANCE_PROVIDED == "true" ]]; then
-    # Gather ArgoCD instance information
-    echo "You have chosen to provide your own ArgoCD instance"
-
+# RHDH Instance Created By Installer
+echo "Applying ArgoCD ConfigMaps"
+if [[ $RHDH_INSTANCE_PROVIDED == "false" ]]; then
+    # Add ConfigMap To Configure ArgoCD
+    kubectl apply -n $NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
+else
     until [ ! -z "${EXISTING_NAMESPACE}" ]; do
-        read -p "Enter the namespace of your RHDH and ArgoCD instance: " EXISTING_NAMESPACE
+        read -p "Enter the namespace of your RHDH instance: " EXISTING_NAMESPACE
         if [ -z "${EXISTING_NAMESPACE}" ]; then
             echo "No namespace entered, try again."
         fi
     done
+    # Create the plugin and config setup configmaps in the namespace
+    kubectl apply -n $EXISTING_NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
+fi
 
-    until [ ! -z "${EXISTING_DEPLOYMENT}" ]; do
-        read -p "Enter the deployment name of your RHDH instance: " EXISTING_DEPLOYMENT
-        if [ -z "${EXISTING_DEPLOYMENT}" ]; then
-            echo "No deployment entered, try again."
-        fi
-    done
-
-    until [ ! -z "${RHDH_PLUGINS}" ]; do
-        read -p "Enter the name of the ConfigMap for your RHDH plugins: " RHDH_PLUGINS
-        if [ -z "${RHDH_PLUGINS}" ]; then
-            echo "No ConfigMap entered, try again."
-        fi
-    done
+# ArgoCD Instance Brought By User
+# If a user is using this step it is assumed they did not use our installer so we need ALL the information required with no assumptions
+if [[ $ARGOCD_INSTANCE_PROVIDED == "true" ]] && [[ -z "$(kubectl get secret -n $NAMESPACE $RHDH_ARGOCD_SECRET --ignore-not-found)" ]]; then
+    # Gather ArgoCD instance information
+    echo "You have chosen to provide your own ArgoCD instance"
 
     until [ ! -z "${ARGO_USERNAME}" ]; do
         read -p "Enter your ArgoCD username: " ARGO_USERNAME
@@ -78,16 +64,23 @@ if [[ $ARGOCD_INSTANCE_PROVIDED == "true" ]]; then
     done
     
     echo "Creating ArgoCD secret file"
-    # Create the secret in the namespace so we can connect it to rhdh TODO: need the setup namespace input
-    kubectl create secret generic "rhdh-argocd-secret" \
-            --from-literal="ARGOCD_API_TOKEN=$ARGO_TOKEN" \
-            --from-literal="ARGOCD_HOSTNAME=$ARGO_HOSTNAME" \
-            --from-literal="ARGOCD_PASSWORD=$ARGO_PASSWORD" \
-            --from-literal="ARGOCD_USER=$ARGO_USERNAME" \
-            -n "$EXISTING_NAMESPACE" \
-            > /dev/null
-    
-    echo "Applying ArgoCD ConfigMaps"
-    # Create the plugin and config setup configmaps in the namespace
-    kubectl apply -n $EXISTING_NAMESPACE -f $BASE_DIR/resources/argocd-config.yaml
+    if [[ $RHDH_INSTANCE_PROVIDED == "false" ]]; then
+        # Create the secret in the namespace so we can connect it to rhdh TODO: need the setup namespace input
+        kubectl create secret generic "$RHDH_ARGOCD_SECRET" \
+                --from-literal="ARGOCD_API_TOKEN=$ARGO_TOKEN" \
+                --from-literal="ARGOCD_HOSTNAME=$ARGO_HOSTNAME" \
+                --from-literal="ARGOCD_PASSWORD=$ARGO_PASSWORD" \
+                --from-literal="ARGOCD_USER=$ARGO_USERNAME" \
+                -n "$NAMESPACE" \
+                > /dev/null
+    else
+        # Create the secret in the namespace so we can connect it to rhdh TODO: need the setup namespace input
+        kubectl create secret generic "$RHDH_ARGOCD_SECRET" \
+                --from-literal="ARGOCD_API_TOKEN=$ARGO_TOKEN" \
+                --from-literal="ARGOCD_HOSTNAME=$ARGO_HOSTNAME" \
+                --from-literal="ARGOCD_PASSWORD=$ARGO_PASSWORD" \
+                --from-literal="ARGOCD_USER=$ARGO_USERNAME" \
+                -n "$EXISTING_NAMESPACE" \
+                > /dev/null
+    fi
 fi
